c4f6f5326d5f3583888e504aa73e29f9f2de8e7b
Ivan Wills <ivan.wills@gmail.com>
2015-05-28 18:37:34 +1000

    Added code

--- lib/Group/Git/Cmd/Todo.pm
+++ lib/Group/Git/Cmd/Todo.pm
@@ -1,40 +1,61 @@
 package Group::Git::Cmd::Todo;
 
-# Created on: 2015-05-28 17:56:05
+# Created on: 2013-05-10 07:05:17
 # Create by:  Ivan Wills
 # $Id$
 # $Revision$, $HeadURL$, $Date$
 # $Revision$, $Source$, $Date$
 
 use strict;
-use warnings;
 use version;
+use Moose::Role;
 use Carp;
-use Scalar::Util;
-use List::Util;
-#use List::MoreUtils;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
-use base qw/Some::Thing/;
+use File::chdir;
+use Path::Tiny;
 
+our $VERSION = version->new('0.0.1');
 
-our $VERSION     = version->new('0.0.1');
-our @EXPORT_OK   = qw//;
-our %EXPORT_TAGS = ();
-#our @EXPORT      = qw//;
+my $opt = Getopt::Alt->new(
+    {
+        helper  => 1,
+        help    => __PACKAGE__,
+        default => {
+            min => 1,
+        },
+    },
+    [
+        'min|min-commits|m=i',
+        'name|n',
+        'no_release|no-release',
+        'verbose|v+',
+        'quiet|q!',
+    ]
+);
 
-sub new {
-	my $caller = shift;
-	my $class  = ref $caller ? ref $caller : $caller;
-	my %param  = @_;
-	my $self   = \%param;
+my $todo = '';
+sub todo {
+    my ($self, $name) = @_;
 
-	bless $self, $class;
+    return unless -d $name;
 
-	return $self;
+    $opt->process if !%{ $opt->opt || {} };
+
+    local $CWD = $name;
+
+    return if !-f 'TODO.md';
+
+    $todo .= "\n# $name\n\n" . path('TODO.md')->slurp;
+
+    return;
 }
 
+sub todo_end {
+    print $todo;
 
+    return;
+}
 
 1;
 
@@ -42,7 +63,7 @@ __END__
 
 =head1 NAME
 
-Group::Git::Cmd::Todo - <One-line description of module's purpose>
+Group::Git::Cmd::Todo - Group-Git tools to show combined markdown TODOs
 
 =head1 VERSION
 
@@ -60,75 +81,19 @@ This documentation refers to Group::Git::Cmd::Todo version 0.0.1
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Just the holder module
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<new ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: Group::Git::Cmd::Todo -
-
-Description:
-
-=cut
-
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
-
 =head1 DEPENDENCIES
-
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -142,7 +107,7 @@ Ivan Wills - (ivan.wills@gmail.com)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+Copyright (c) 2013 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
 All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under
